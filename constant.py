7826cd27cfad61414e1b3e1c8cbbd93a1d6fa3b4
Xunino <ndlinh.ai@gmail.com>
2021-09-03 04:08:09 +0700

    changed something

--- constant.py
+++ constant.py
@@ -43,39 +43,17 @@ class Bleu_score:
             return score
 
 
-class MaskedSoftmaxCELoss(tf.keras.losses.Loss):
+def MaskedSoftmaxCELoss(label, pred):
     """
-        The softmax cross-entropy loss with masks.
+    :param label: shape (batch_size, max_length, vocab_size)
+    :param pred: shape (batch_size, max_length)
 
-        Tính giá trị mất mát tập chung các vị trí xuất hiện (Y_hat) từ giống với giá trị gốc (Y_true):
-
-        Ví dụ cơ bản:
-            Đầu vào:
-                pred_matrix = [1, 25, 1445, 105, 5, 4, 8, 2]
-                true_matrix = [0, 20, 1456, 145, 2, 0, 0, 0]
-
-            Chỗ nào có giá trị khác 0 tại true_matrix gán bằng giá trị 1:
-            mask_matrix = [0, 1, 1, 1, 1, 0, 0, 0]
-
-            loss = true_matrix - pred_matrix = [1, 0, 11, 40, 0, 4, 8, 2]
-            ==> loss = loss * mask_matrix = [0, 5, 11, 40, 3, 0, 0, 0]
+    :return: weighted_loss: shape (batch_size, max_length)
     """
-
-    def __init__(self):
-        super().__init__()
-
-    def call(self, label, pred):
-        """
-        :param label: shape (batch_size, max_length, vocab_size)
-        :param pred: shape (batch_size, max_length)
-
-        :return: weighted_loss: shape (batch_size, max_length)
-        """
-
-        weights_mask = 1 - np.equal(label, 0)
-        unweighted_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(label, pred)
-        weighted_loss = tf.reduce_mean(unweighted_loss * weights_mask)
-        return weighted_loss
+    weights_mask = 1 - np.equal(label, 0)
+    unweighted_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(label, pred)
+    weighted_loss = tf.reduce_mean(unweighted_loss * weights_mask)
+    return weighted_loss
 
 
 class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
